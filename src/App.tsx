f7da9b0de1fc1915e1d8aad885149df261c02f29
JayoonKoo <rnwkdbs12@gmail.com>
2021-09-27 15:46:59 +0900

    feat: router 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-
+import {BrowserRouter, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/">
+					<h1>메인 페이지 입니다.</h1>
+				</Route>
+				<Route path="/words">
+					<h1>단어 페이지 입니다.</h1>
+				</Route>
+				<Route path="/create_word">
+					<h1>단어 생성 페이지입니다.</h1>
+				</Route>
+				<Route>
+					<h1>잘못된 url 입니다.</h1>
+				</Route>
+			</Switch>
+		</BrowserRouter>
   );
 }
 
