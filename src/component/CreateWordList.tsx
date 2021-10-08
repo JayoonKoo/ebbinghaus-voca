8c510b9f25da30a8f71aa8001b1dff51de335363
JayoonKoo <rnwkdbs12@gmail.com>
2021-10-08 15:32:39 +0900

    feat: 단어 생성 리스트 만든 후 생성 기능 완성

--- src/component/CreateWordList.tsx
+++ src/component/CreateWordList.tsx
@@ -4,39 +4,74 @@ import { IWord } from "types";
 
 const CreateWordList = () => {
   const [addingWords, setAddingWords] = useState<IWord[]>([]);
-	const [wordId, setWordId] = useState(0);
-	const [day, setDay] = useState("1");
+  const [wordId, setWordId] = useState(0);
+  const [day, setDay] = useState("1");
   const [eng, setEng] = useState("");
   const [kor, setKor] = useState("");
-	const url = "http://localhost:3001/days";
-	const [days, ] = useFetch<IWord>(url);
+  const url = "http://localhost:3001/days";
+	const wordUrl = "http://localhost:3001/words";
+  const [days] = useFetch<IWord>(url);
 
-	const inputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const {target: {name, value}} = e;
-		name === 'kor' ? setKor(value) : setEng(value);
-	}
+  const inputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    name === "kor" ? setKor(value) : setEng(value);
+  };
 
-	const createWord = () => {
-		console.log(wordId);
-		const newWord:IWord = {
-			id: wordId,
-			day: Number(day), 
-			eng,
-			kor,
-			isDone: false,
+  const createWord = () => {
+		if (!eng || !kor) {
+			alert("입력란을 채워주세요.");
+			return;
 		}
+    const newWord: IWord = {
+      id: wordId,
+      day: Number(day),
+      eng,
+      kor,
+      isDone: false,
+    };
 
-		const newWords = addingWords.slice(0);
-		newWords.push(newWord);
-		setAddingWords(newWords);
-		setWordId(prev => prev + 1);
-		setEng("");
-		setKor("");
-	}
+    const newWords = addingWords.slice(0);
+    newWords.push(newWord);
+    setAddingWords(newWords);
+    setWordId((prev) => prev + 1);
+    setEng("");
+    setKor("");
+  };
 
-	const onSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-		const {target: {value: day}} = event;
-		setDay(day);
+  const onSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const {
+      target: { value: day },
+    } = event;
+    setDay(day);
+  };
+
+	const onSubmit = async (event: React.FormEvent) => {
+		event.preventDefault();
+		try {
+			for (let word of addingWords) {
+				const {day, eng, kor, isDone} = word;
+				await fetch(wordUrl, {
+					method: 'POST',
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						day: Number(day),
+						eng,
+						kor,
+						isDone,
+					})
+				})
+			}
+			setAddingWords([]);
+			setWordId(0);
+			setEng("");
+			setKor("");
+		} catch (error) {
+			console.log(error);
+		}
 	}
 
   return (
@@ -45,31 +80,45 @@ const CreateWordList = () => {
         <ul>
           {addingWords.map((word) => (
             <li key={word.id}>
-							<div>
-								<span>{word.day}</span>
-								<span>{word.eng}</span>
-								<span>{word.kor}</span>
-							</div>
-						</li>
+              <div>
+                <span>{word.day}</span>
+                <span>{word.eng}</span>
+                <span>{word.kor}</span>
+              </div>
+            </li>
           ))}
         </ul>
       </div>
       <div>
-				<select value={day} onChange={onSelectChange}>
-					{days.map((mday) => (
-						<option key={mday.id} value={mday.day}>
-							{mday.day}
-						</option>
-					))}
-				</select>
-        <input value={eng} name="eng" onChange={inputChange} placeholder="단어를 입력하세요." required />
-        <input value={kor} name="kor" onChange={inputChange} placeholder="뜻을 입력하세요." required />
-				<button onClick={createWord}>추가하기</button>
+        <select value={day} onChange={onSelectChange}>
+          {days.map((mday) => (
+            <option key={mday.id} value={mday.day}>
+              {mday.day}
+            </option>
+          ))}
+        </select>
+        <input
+          value={eng}
+					type="text"
+          name="eng"
+          onChange={inputChange}
+          placeholder="단어를 입력하세요."
+          required={true}
+        />
+        <input
+          value={kor}
+					type="text"
+          name="kor"
+          onChange={inputChange}
+          placeholder="뜻을 입력하세요."
+          required={true}
+        />
+        <button onClick={createWord}>추가하기</button>
       </div>
 
-			<form>
-				<input type="submit" value="생성하기"/>
-			</form>
+      <form onSubmit={onSubmit}>
+        <input type="submit" value="생성하기" />
+      </form>
     </>
   );
 };
